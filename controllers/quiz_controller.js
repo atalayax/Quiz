9ad2ba6ba93ab0e01bd3893f2f2a141a59232962
MALopez <honeychickenfirst@gmail.com>
2015-08-15 17:41:52 +0100

    Conflicts

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,30 +17,14 @@ exports.load = function(req, res, next, quizId) {
 
 /* GET /quizes */
 exports.index = function(req, res) {
-  if (req.query.search) {
-    var search = "%" + req.query.search.replace(" ", "%") + "%";
-    models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
-      res.render(
-        "quizes/index",
-        { quizes: quizes}
-      );
-    }).catch(function(error) {
-      next(error);
-    });
-  }
-  else {
-    models.Quiz.findAll().then(function(quizes) {
-      res.render(
-        "quizes/index",
-        { quizes: quizes}
-      );
-    }).catch(function(error) {
-      next(error);
-    });
-  }
-  var renderIndex = function() {
-
-  };
+  models.Quiz.findAll().then(function(quizes) {
+    res.render(
+      "quizes/index",
+      { quizes: quizes}
+    );
+  }).catch(function(error) {
+    next(error);
+  });
 };
 
 /* GET /quizes/:id */
@@ -54,7 +38,7 @@ exports.pregunta = function(req, res) {
 /* GET /quizes/:id/respuesta */
 exports.respuesta = function(req, res) {
   var resultado = "Incorrecto";
-  if (req.query.respuesta && req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
+  if (req.query.respuesta && req.query.respuesta.toLowerCase() === quiz.respuesta.toLowerCase()) {
     resultado = "Correcto!";
   }
   res.render(
